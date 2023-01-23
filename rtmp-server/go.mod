bc0801c8110a38e4252276d29ad95aab8f23c978
AgustinSRG <agustinsanromanguzman@gmail.com>
2023-01-23 16:35:45 +0100

    rtmp: dotenv

--- rtmp-server/go.mod
+++ rtmp-server/go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/gorilla/websocket v1.5.0
+	github.com/joho/godotenv v1.4.0
 	github.com/netdata/go.d.plugin v0.49.1
 )
 
--- rtmp-server/main.go
+++ rtmp-server/main.go
@@ -1,6 +1,12 @@
 package main
 
+import "github.com/joho/godotenv"
+
 func main() {
+	godotenv.Load() // Load env vars
+
+	InitLog()
+
 	LogInfo("RTMP-Server (Version 1.0.0)")
 
 	server := CreateRTMPServer()
