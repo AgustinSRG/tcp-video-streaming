f2e80a78e61f941e9ed7c35cb282e2d0fb16ab9e
AgustinSRG <agustinsanromanguzman@gmail.com>
2023-01-31 10:14:29 +0100

    hls: Add dotenv

--- hls-encoder/go.mod
+++ hls-encoder/go.mod
@@ -1,3 +1,5 @@
 module github.com/AgustinSRG/tcp-video-streaming/hls-encoder
 
 go 1.17
+
+require github.com/joho/godotenv v1.4.0
--- hls-encoder/main.go
+++ hls-encoder/main.go
@@ -2,6 +2,14 @@
 
 package main
 
+import "github.com/joho/godotenv"
+
+const VERSION = "1.0.0"
+
 func main() {
-	LogInfo("HLS encoder worker (Version 1.0.0)")
+	godotenv.Load() // Load env vars
+
+	InitLog()
+
+	LogInfo("Started HLS encoder worker - Version " + VERSION)
 }
