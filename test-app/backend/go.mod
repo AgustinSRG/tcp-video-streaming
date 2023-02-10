f6a8f9024002122b0a7e64cacfead981a0b6810c
AgustinSRG <agustinsanromanguzman@gmail.com>
2023-02-10 09:10:28 +0100

    test: Add dotenv

--- test-app/backend/go.mod
+++ test-app/backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/AgustinSRG/tcp-video-streaming/test-app/backend
 
 go 1.17
+
+require github.com/joho/godotenv v1.5.1
--- test-app/backend/main.go
+++ test-app/backend/main.go
@@ -2,7 +2,11 @@
 
 package main
 
+import "github.com/joho/godotenv"
+
 func main() {
+	godotenv.Load() // Load env vars
+
 	InitLog()
 
 	LogInfo("Started streaming test backend server")
