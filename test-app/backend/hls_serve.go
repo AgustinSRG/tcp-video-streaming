499ed2dc52de23856a78f0bbf549cfe837c7ca08
AgustinSRG <agustinsanromanguzman@gmail.com>
2023-02-23 09:45:45 +0100

    test-app: Set mime types for HLS files

--- test-app/backend/hls_serve.go
+++ test-app/backend/hls_serve.go
@@ -45,6 +45,7 @@ func hls_servePlaylist(response http.ResponseWriter, request *http.Request) {
 	fullPath := filepath.Join(basePath, "hls", channel, streamId, resolution.Encode(), fileName)
 
 	response.Header().Add("Cache-Control", "no-cache") // No cache for playlists
+	response.Header().Add("Content-Type", "application/x-mpegURL")
 
 	http.ServeFile(response, request, fullPath)
 }
@@ -85,6 +86,7 @@ func hls_serveFragment(response http.ResponseWriter, request *http.Request) {
 	fullPath := filepath.Join(basePath, "hls", channel, streamId, resolution.Encode(), fileName)
 
 	response.Header().Add("Cache-Control", "max-age=31536000")
+	response.Header().Add("Content-Type", "video/MP2T")
 
 	http.ServeFile(response, request, fullPath)
 }
@@ -125,6 +127,7 @@ func hls_servePreviewsIndex(response http.ResponseWriter, request *http.Request)
 	fullPath := filepath.Join(basePath, "img-preview", channel, streamId, resolution.Encode(), fileName)
 
 	response.Header().Add("Cache-Control", "no-cache") // No cache for index files
+	response.Header().Add("Content-Type", "application/json")
 
 	http.ServeFile(response, request, fullPath)
 }
@@ -165,6 +168,7 @@ func hls_servePreviewImage(response http.ResponseWriter, request *http.Request) 
 	fullPath := filepath.Join(basePath, "img-preview", channel, streamId, resolution.Encode(), fileName)
 
 	response.Header().Add("Cache-Control", "max-age=31536000")
+	response.Header().Add("Content-Type", "image/jpeg")
 
 	http.ServeFile(response, request, fullPath)
 }
